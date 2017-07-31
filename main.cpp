12820fd33be492f450f5ca5fab94891aa25cc5ba
Siphalor <xyx0125@gmail.com>
2017-07-31 18:52:05 +0200

    Moved spritesheet to img

--- main.cpp
+++ main.cpp
@@ -11,23 +11,23 @@ int main()
 	const Vector2u wSize = Vector2u(14 * 48, 14 * 48);
 	RenderWindow window(VideoMode(wSize.x, wSize.y, 32), "TextureReaderTest");
 	Event event;
-	
+
 	ChessTextureReader texturePack;
-	
-	if(!texturePack.load("spritesheet.png"))
+
+	if(!texturePack.load("img/spritesheet.png"))
 		return -1;
-	
+
 	Sprite sprites[14];
-	
+
 	for(int i = 0; i < 14; i++)
 	{
 		sprites[i].setTexture(texturePack.get(i, true));
-		
+
 		sprites[i].setScale(3, 3);
-		
+
 		sprites[i].setPosition(i* 48, i * 48);
 	}
-	
+
 	while(window.isOpen())
 	{
 		while(window.pollEvent(event))
@@ -35,17 +35,17 @@ int main()
 			if(event.type == Event::Closed)
 				window.close();
 		}
-		
+
 		if(window.getSize().x != wSize.x || window.getSize().y != wSize.y)
 			window.setSize(wSize);
-		
+
 		window.clear(Color::White);
 		for(int i = 0; i < 14; i++)
 		{
 			window.draw(sprites[i]);
 		}
 		window.display();
-	}	
-	
+	}
+
 	return 0;
 }
